ec059f8e7b8670b9d5e2de605fcdcdcc6de32f5a
bjoern <bjoern.quentin@mobile-j.de>
2021-05-20 18:03:31 +0200

    add ilp32f support

--- assemble.sh
+++ assemble.sh
@@ -13,6 +13,9 @@ ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
 riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32ic asm.S -o bin/$crate.o
 ar crs bin/riscv32ic-unknown-none-elf.a bin/$crate.o
 
+riscv64-unknown-elf-gcc -c -mabi=ilp32f -march=rv32imfc asm.S -o bin/$crate.o
+ar crs bin/riscv32imfc-unknown-none-elf.a bin/$crate.o
+
 riscv64-unknown-elf-gcc -c -mabi=lp64 -march=rv64i asm.S -o bin/$crate.o
 ar crs bin/riscv64i-unknown-none-elf.a bin/$crate.o
 
